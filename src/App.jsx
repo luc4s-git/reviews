45197292febf4f8a694561bdc66f38447e6553cb
luc4s-git <lucassantos4332@hotmail.com>
2023-11-27 00:29:44 -0300

    functions optmization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import reviews from "./data";
+import person from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const App = () => {
-  const [person, setPerson] = useState(reviews);
   const [index, setIndex] = useState(0);
+  const { id, name, job, image, text } = person[index];
 
   const previousPerson = () => {
     if (index <= 0) return;
@@ -16,22 +16,26 @@ const App = () => {
     setIndex(index + 1);
   };
 
+  const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const getRandomPerson = () => {
+    setIndex(getRandomNumber(0, person.length));
+  };
+
   return (
     <main>
       <article className="review">
         <div className="img-container">
-          <img
-            src={person[index].image}
-            alt={person[index].name}
-            className="person-img"
-          />
+          <img src={image} alt={name} className="person-img" />
           <span className="quote-icon">
             <FaQuoteRight />
           </span>
         </div>
-        <h4 className="author">{person[index].name}</h4>
-        <p className="job">{person[index].job}</p>
-        <p className="info">{person[index].text}</p>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
         <div className="btn-container">
           <button className="prev-btn" onClick={previousPerson}>
             <FaChevronLeft />
@@ -40,16 +44,7 @@ const App = () => {
             <FaChevronRight />
           </button>
           <div>
-            <button
-              className="btn"
-              onClick={() => {
-                const getRandomNumber = (min, max) => {
-                  return Math.floor(Math.random() * (max - min) + min);
-                };
-
-                setIndex(getRandomNumber(0, person.length));
-              }}
-            >
+            <button className="btn" onClick={getRandomPerson}>
               surprise me!
             </button>
           </div>
